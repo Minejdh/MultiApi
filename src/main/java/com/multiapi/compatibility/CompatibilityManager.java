9bccf5bc9aa996107eec285827c565061ed5007c
Minejdh <130380206+Minejdh@users.noreply.github.com>
2026-06-09 12:16:55 -0300

    Create CompatibilityManager.java

---
+++ src/main/java/com/multiapi/compatibility/CompatibilityManager.java
@@ -0,0 +1,11 @@
+package com.multiapi.compatibility;
+
+public class CompatibilityManager {
+
+    public static void initialize() {
+
+        System.out.println(
+            "[MultiApi] Compatibility layer initialized"
+        );
+    }
+}
